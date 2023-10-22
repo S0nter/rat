7e03feb5472a4ddf171697c28ee25d82204cf243
Sonter <108224581+S0nter@users.noreply.github.com>
2023-10-22 15:33:51 +0000

    Use command line arguments

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    string name = "file.rat";
+    cout << argv[1] << endl;
+    if (argc <= 1) {
+        std::cout << "Usage: " << argv[0] <<" file.rat out" << endl;
+        return 1;
+    }
+    string name(argv[1]);
     fstream file;
-    cout << "File name: ";
-    cout << name << endl; //cin >> name;
+    cout << "File name: " << name << endl;;
     file.open("./" + name, ios_base::in);
     if (!file.is_open())
     {
@@ -24,10 +28,10 @@ int main()
 
     string output = Compile(content);
     fstream outputfile;
-    outputfile.open("./output.asm", ios_base::out);
+    outputfile.open(argv[2], ios_base::out);
     if (!outputfile.is_open())
     {
-        cout << "Cannot open \33[1;91m\"output.asm\"\33[0m" << endl;
+        cout << "Cannot open \33[1;91m\"" << argv[2] << "\"\33[0m" << endl;
         return 1;
     }
     outputfile << output;
