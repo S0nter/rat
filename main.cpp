1ac76dff65b2eadeb96b3c05499b0680fd7d9d4c
rodentous <98844072+rodentous@users.noreply.github.com>
2023-10-22 15:52:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ int main()
         cout << "Cannot open \33[1;91m\"output.asm\"\33[0m" << endl;
         return 1;
     }
+    cout << output;
     outputfile << output;
     outputfile.close();
     cout << "SUCCESS";
